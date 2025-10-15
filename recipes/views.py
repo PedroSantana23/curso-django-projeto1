a4b814809e393cd9aa31c72ffb1a3360b0d5a0ad
PedroSantana23 <ph.silva.santana@gmail.com>
2025-10-15 12:08:53 -0300

    Seção 10: Usando request.GET no Django e Introdução ao TDD (Test Driven Development)

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from utils.recipes.factory import make_recipe
+from django.db.models import Q
 from recipes.models import Recipe
+from django.http import Http404
 
 
 def home(request):
@@ -29,4 +30,25 @@ def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
+    })
+
+def search(request):
+    search_term = request.GET.get('q', '').strip()  
+
+    if not search_term:
+        raise Http404()
+    
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) | 
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+    
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" | ',
+        'search_term': search_term,
+        'recipes': recipes,
     })
